c261f9271227610740e667ce590fa2256b746a84
rahulnl <rahul.sharma@hiro-microdatacenters.nl>
2025-07-07 11:21:33 +0200

    dev build

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:30015',
+  tokenKey: 'auth_token',
+  refreshTokenKey: 'refresh_token',
+  userKey: 'user',
+  dashboardUrl: 'http://localhost:30016',
+  cogUrl: 'https://dashboard.cog.hiro-develop.nl/cogui/',
+}; 
